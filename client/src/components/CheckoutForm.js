d6bf4ec514b7f7e26fa50a1dd1953d9aef28e340
Bill Xiong <xiong.bill@yahoo.com>
2020-11-08 20:15:34 -0800

    MVP

--- client/src/components/CheckoutForm.js
+++ client/src/components/CheckoutForm.js
@@ -1,30 +1,14 @@
 import React, { useState } from "react";
+import {useForm} from "../hooks/useForm";
 
-const initialValue = {
-  firstName: "",
-  lastName: "",
-  address: "",
-  city: "",
-  state: "",
-  zip: "",
-};
 
 // This form should be handled by a "useForm" custom hook
 // Build out the logic needed for a form custom hook (see the useForm.js file)
 // and replace the necessary stateful logic from CheckoutForm with the hook
 
 const CheckoutForm = (props) => {
-  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  const [values, setValues] = useState(initialValue);
 
-  const handleChanges = (e) => {
-    setValues({ ...values, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setShowSuccessMessage(true);
-  };
+  const [values, showSuccessMessage, handleChanges, handleSubmit] = useForm();
 
   return (
     <>
@@ -34,6 +18,7 @@ const CheckoutForm = (props) => {
           First Name:
           <input
             name="firstName"
+            placeholder="firstName"
             value={values.firstName}
             onChange={handleChanges}
           />
@@ -42,6 +27,7 @@ const CheckoutForm = (props) => {
           Last Name:
           <input
             name="lastName"
+            placeholder="lastName"
             value={values.lastName}
             onChange={handleChanges}
           />
@@ -50,21 +36,22 @@ const CheckoutForm = (props) => {
           Address:
           <input
             name="address"
+            placeholder="address"
             value={values.address}
             onChange={handleChanges}
           />
         </label>
         <label>
           City:
-          <input name="city" value={values.city} onChange={handleChanges} />
+          <input name="city" placeholder="city" value={values.city} onChange={handleChanges} />
         </label>
         <label>
           State:
-          <input name="state" value={values.state} onChange={handleChanges} />
+          <input name="state" placeholder="state" value={values.state} onChange={handleChanges} />
         </label>
         <label>
           Zip:
-          <input name="zip" value={values.zip} onChange={handleChanges} />
+          <input name="zip" placeholder="zip" value={values.zip} onChange={handleChanges} />
         </label>
         <button>Checkout</button>
       </form>
--- client/src/components/PlantList.js
+++ client/src/components/PlantList.js
@@ -1,12 +1,38 @@
 import React, { Component } from "react";
 import axios from "axios";
 
-export default class PlantList extends Component {
-  // add state with a property called "plants" - initialize as an empty array
+class PlantList extends React.Component {
+
+// add state with a property called "plants" - initialize as an empty array
+  constructor() {
+    super();
+    this.state = {
+      plants: []
+    };
+  }
 
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
   //   - set the returned plants array to this.state.plants
+
+  componentDidMount() {
+    this.fetchPlants();
+  }
+
+  fetchPlants = () => {
+    fetch(`http://localhost:3333/plants`)
+      .then((res) => res.json())
+      .then((data) => {
+        this.setState({
+          plants: data.plantsData,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  
+  
 
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
   render() {
@@ -37,3 +63,5 @@ export default class PlantList extends Component {
     );
   }
 }
+
+export default PlantList;
